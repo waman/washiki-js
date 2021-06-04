3d37e9fea00184a59b2097fcbe50ff12ba403a4a
waman <waman10da@gmail.com>
2021-06-05 06:29:00 +0900

    first commit

---
+++ src/MyTestSuite.spec.ts
@@ -0,0 +1,12 @@
+import {expect, Test, TestSuite} from 'testyts';
+
+@TestSuite()
+export class MyTestSuite{
+    @Test()
+    onePlusOne(){
+        // Act
+        const result = 1 + 1;
+        // Assert
+        expect.toBeEqual(result, 2);
+    }
+}
